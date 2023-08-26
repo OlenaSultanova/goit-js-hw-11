7338dd1e6dfef7ab683fecce2cbadd1bae6d36a3
OlenaSultanova <galeodeahelen@gmail.com>
2023-08-26 16:20:42 +0300

    update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -4,7 +4,19 @@ const URL = 'https://pixabay.com/api/';
 const KEY = '39060252-4018ed750f30cc123247f9a4e';
 
 export async function fetchPhoto(q, page, perPage) {
-  const url = `${URL}?key=${KEY}&q=${q}&page=${page}&per_page=${perPage}&image_type=photo&orientation=horizontal&safesearch=true`;
-  const response = await axios.get(url);
+  const params = {
+    key: KEY,
+    q: q,
+    page: page,
+    per_page: perPage,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+  };
+
+  const response = await axios.get(URL, {
+    params: params,
+  });
+
   return response.data;
 }
